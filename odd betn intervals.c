0ea7f80ab12dd46e4fb003dfeb40e7671fd2859d
bishal86 <35268541+bishal86@users.noreply.github.com>
2018-01-16 18:52:30 +0530

    Add files via upload

---
+++ odd betn intervals.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+void odd(int f, int l)
+{
+	int i;
+	for(i=f;i<=l;i++)
+	{
+		if(i%2!=0)
+		printf("%d\t",i);
+	}
+}
+	
+int main()
+{
+	int f,l;
+	printf("Enter the first and last interval");
+	scanf("%d%d",&f,&l);
+	odd(f,l);
+	return 0;
+	
+	
+}
