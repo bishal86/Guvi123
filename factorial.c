b610c35a7d2aedd65a3cee97d0b70d1d0f9ed454
bishal86 <35268541+bishal86@users.noreply.github.com>
2018-01-16 19:26:38 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -2,16 +2,22 @@
 void fact(int n)
 {
 	int i,f=1;
-    for(i=1;i<=n;i++)
-    f=f*i;
-    printf("Factorial of %d is %d",n,f);
+        if(n<0)
+   	printf("No factorial");
+   	else if(n==0)
+   	printf("factorial is 0");
+   	else
+   	{
+   	 for(i=1;i<=n;i++)
+   	 f=f*i;
+         printf("Factorial of %d is %d",n,f);
+  }
 }
 int main()
 {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    fact(n);
-    return 0;
+      int n;
+      printf("Enter a number: ");
+      scanf("%d",&n);
+      fact(n);
+      return 0;
 }
-
