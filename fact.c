b0f6db600660c3047676eec1d8b046f8b163d381
bishal86 <35268541+bishal86@users.noreply.github.com>
2018-01-17 22:35:25 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void fact(int n)
+{
+	int i,f=1;
+    if(n<0)
+   	printf("No factorial");
+   	else if(n==0)
+   	printf("factorial is 0");
+   	else
+   	{
+   	 for(i=1;i<=n;i++)
+   	 f=f*i;
+         printf("Factorial of %d is %d",n,f);
+  }
+}
+int main()
+{
+      int n;
+      printf("Enter a number: ");
+      scanf("%d",&n);
+      fact(n);
+      return 0;
+}
+
