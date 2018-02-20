3cf02c5f0424429eebed2046f39afa1703f79222
bishal86 <35268541+bishal86@users.noreply.github.com>
2018-02-20 22:13:48 +0530

    Add files via upload

---
+++ Binary to dec.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+ 
+ 
+void bin_dec(long int num)  
+{
+long int rem,sum=0,power=0;
+while(num>0)
+ {
+ rem = num%10;
+ num = num/10;
+ sum = sum + rem * pow(2,power);
+ power++;
+ }
+ 
+printf("Decimal number : %d",sum);
+}
+
+int main()
+{
+long int num;
+
+ 
+printf("Enter the Binary number (0 and 1): ");
+scanf("%ld",&num);
+ 
+bin_dec(num);
+ 
+return 0;
+}
